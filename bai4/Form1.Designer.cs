2c4451c7be3e8848106d5fd3e28ce3af7da13b12
hhn1810 <nguyenha018199@gmail.com>
2020-11-26 23:02:22 +0700

    commit

---
+++ bai4/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace bai4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.dtpNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbxNam = new System.Windows.Forms.CheckBox();
+            this.cbxNu = new System.Windows.Forms.CheckBox();
+            this.lblHoTen = new System.Windows.Forms.Label();
+            this.lblSDT = new System.Windows.Forms.Label();
+            this.lblGioiTinh = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("#9Slide03 AmpleSoft", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(197, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(398, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐĂNG KÝ THÀNH VIÊN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("#9Slide03 AmpleSoft", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 153);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Họ Tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("#9Slide03 AmpleSoft", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 199);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ngày Sinh";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("#9Slide03 AmpleSoft", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 249);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Số Điện Thoại";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(153, 150);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(557, 26);
+            this.txtHoTen.TabIndex = 4;
+            this.txtHoTen.TextChanged += new System.EventHandler(this.HoTen_Changed);
+            this.txtHoTen.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtHoTen_KeyPress);
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(153, 246);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(557, 26);
+            this.txtSDT.TabIndex = 5;
+            this.txtSDT.TextChanged += new System.EventHandler(this.txtSDT_TextChanged);
+            this.txtSDT.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSDT_KeyPress);
+            // 
+            // dtpNgaySinh
+            // 
+            this.dtpNgaySinh.Location = new System.Drawing.Point(153, 199);
+            this.dtpNgaySinh.Name = "dtpNgaySinh";
+            this.dtpNgaySinh.Size = new System.Drawing.Size(557, 26);
+            this.dtpNgaySinh.TabIndex = 6;
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Location = new System.Drawing.Point(322, 369);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(146, 52);
+            this.btnDangKy.TabIndex = 7;
+            this.btnDangKy.Text = "Đăng Ký";
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("#9Slide03 AmpleSoft", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(27, 301);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 19);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Giới Tính";
+            // 
+            // cbxNam
+            // 
+            this.cbxNam.AutoSize = true;
+            this.cbxNam.Checked = true;
+            this.cbxNam.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbxNam.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cbxNam.Location = new System.Drawing.Point(153, 301);
+            this.cbxNam.Name = "cbxNam";
+            this.cbxNam.Size = new System.Drawing.Size(64, 23);
+            this.cbxNam.TabIndex = 9;
+            this.cbxNam.Text = "Nam";
+            this.cbxNam.UseVisualStyleBackColor = true;
+            this.cbxNam.CheckedChanged += new System.EventHandler(this.cbxNam_CheckedChanged);
+            // 
+            // cbxNu
+            // 
+            this.cbxNu.AutoSize = true;
+            this.cbxNu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cbxNu.Location = new System.Drawing.Point(322, 300);
+            this.cbxNu.Name = "cbxNu";
+            this.cbxNu.Size = new System.Drawing.Size(50, 23);
+            this.cbxNu.TabIndex = 10;
+            this.cbxNu.Text = "Nữ";
+            this.cbxNu.UseVisualStyleBackColor = true;
+            this.cbxNu.CheckedChanged += new System.EventHandler(this.cbxNu_CheckedChanged);
+            // 
+            // lblHoTen
+            // 
+            this.lblHoTen.AutoSize = true;
+            this.lblHoTen.Font = new System.Drawing.Font("#9Slide03 AmpleSoft", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoTen.Location = new System.Drawing.Point(158, 177);
+            this.lblHoTen.Name = "lblHoTen";
+            this.lblHoTen.Size = new System.Drawing.Size(43, 16);
+            this.lblHoTen.TabIndex = 12;
+            this.lblHoTen.Text = "label7";
+            // 
+            // lblSDT
+            // 
+            this.lblSDT.AutoSize = true;
+            this.lblSDT.Font = new System.Drawing.Font("#9Slide03 AmpleSoft", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSDT.Location = new System.Drawing.Point(158, 275);
+            this.lblSDT.Name = "lblSDT";
+            this.lblSDT.Size = new System.Drawing.Size(43, 16);
+            this.lblSDT.TabIndex = 13;
+            this.lblSDT.Text = "label7";
+            // 
+            // lblGioiTinh
+            // 
+            this.lblGioiTinh.AutoSize = true;
+            this.lblGioiTinh.Font = new System.Drawing.Font("#9Slide03 AmpleSoft", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGioiTinh.Location = new System.Drawing.Point(158, 337);
+            this.lblGioiTinh.Name = "lblGioiTinh";
+            this.lblGioiTinh.Size = new System.Drawing.Size(43, 16);
+            this.lblGioiTinh.TabIndex = 14;
+            this.lblGioiTinh.Text = "label7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 447);
+            this.Controls.Add(this.lblGioiTinh);
+            this.Controls.Add(this.lblSDT);
+            this.Controls.Add(this.lblHoTen);
+            this.Controls.Add(this.cbxNu);
+            this.Controls.Add(this.cbxNam);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.dtpNgaySinh);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("#9Slide03 AmpleSoft", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form Đăng Ký";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.DateTimePicker dtpNgaySinh;
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox cbxNam;
+        private System.Windows.Forms.CheckBox cbxNu;
+        private System.Windows.Forms.Label lblHoTen;
+        private System.Windows.Forms.Label lblSDT;
+        private System.Windows.Forms.Label lblGioiTinh;
+    }
+}
+
---
+++ bai4/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDangKy_Click(object sender, EventArgs e)
+        {
+            string hoTen = txtHoTen.Text;
+            string ngaySinh = dtpNgaySinh.Value.ToString();
+            string sdt = txtSDT.Text;
+            string gioitinh = "";
+            int err = 0;
+            if(hoTen == "")
+            {
+                lblHoTen.ForeColor = Color.Red;
+                lblHoTen.Text = "Họ tên không được bỏ trống";
+                err++;
+            }else
+            {
+                string str = txtHoTen.Text.Trim();
+                Regex trimmer = new Regex(@"\s\s+");
+                str = trimmer.Replace(str, " ");
+                string[] arrString = str.Split(' ');
+                if(arrString.Length < 2)
+                {
+                    lblHoTen.ForeColor = Color.Red;
+                    lblHoTen.Text = "Họ tên phải có ít nhất 2 từ";
+                }
+                else
+                {
+                    lblHoTen.ForeColor = Color.Green;
+                    lblHoTen.Text = "OK!!!";
+                }
+                
+            }
+            if(sdt == "")
+            {
+                lblSDT.ForeColor = Color.Red;
+                lblSDT.Text = "Số điện thoại không được bỏ trống";
+                err++;
+            }else if(sdt.Length != 10)
+            {
+                lblSDT.ForeColor = Color.Red;
+                lblSDT.Text = "Số điện thoại phải có 10 số";
+                err++;
+            }
+            else
+            {
+                lblSDT.ForeColor = Color.Green;
+                lblSDT.Text = "OK!!!";
+            }
+            if(cbxNam.Checked == false && cbxNu.Checked == false)
+            {
+                lblGioiTinh.ForeColor = Color.Red;
+                lblGioiTinh.Text = "Bạn phải check nam hoặc nữ";
+                err++;
+            }
+            else
+            {
+                lblGioiTinh.Text = "";
+            }
+            if(err == 0)
+            {
+                if(cbxNam.Checked == true)
+                {
+                    gioitinh += cbxNam.Text;
+                }
+                else
+                {
+                    gioitinh += cbxNu.Text;
+                }
+                string chuoi = "Họ Tên: " + hoTen + "\n" + "Ngày Sinh: " + ngaySinh + "\n" + " SĐT: " + sdt + "\n" + "Giới Tính: " + gioitinh;
+                MessageBox.Show(chuoi, "Kết Quả", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblGioiTinh.Text = "";lblHoTen.Text = "";lblSDT.Text = "";
+        }
+
+        private void cbxNam_CheckedChanged(object sender, EventArgs e)
+        {
+            if(cbxNam.CheckState == CheckState.Checked)
+            {
+                cbxNu.Checked = false;
+                cbxNu.CheckState = CheckState.Unchecked;
+            }
+
+            if (cbxNam.Checked == false && cbxNu.Checked == false)
+            {
+                lblGioiTinh.ForeColor = Color.Red;
+                lblGioiTinh.Text = "Bạn phải check nam hoặc nữ";
+            }
+            else
+            {
+                lblGioiTinh.Text = "";
+            }
+        }
+
+        private void cbxNu_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbxNu.CheckState == CheckState.Checked)
+            {
+                cbxNam.Checked = false;
+                cbxNam.CheckState = CheckState.Unchecked;
+            }
+            if (cbxNam.Checked == false && cbxNu.Checked == false)
+            {
+                lblGioiTinh.ForeColor = Color.Red;
+                lblGioiTinh.Text = "Bạn phải check nam hoặc nữ";
+            }
+            else
+            {
+                lblGioiTinh.Text = "";
+            }
+        }
+
+        private void HoTen_Changed(object sender, EventArgs e)
+        {
+            if (txtHoTen.Text == "")
+            {
+                lblHoTen.ForeColor = Color.Red;
+                lblHoTen.Text = "Họ tên không được bỏ trống";
+            }
+            else
+            {
+                string str = txtHoTen.Text.Trim();
+                Regex trimmer = new Regex(@"\s\s+");
+                str = trimmer.Replace(str, " ");
+                string[] arrString = str.Split(' ');
+                if(arrString.Length < 2)
+                {
+                    lblHoTen.ForeColor = Color.Red;
+                    lblHoTen.Text = "Họ tên phải có ít nhất 2 từ";
+                }
+                else
+                {
+                    lblHoTen.ForeColor = Color.Green;
+                    lblHoTen.Text = "OK!!!";
+                }
+               
+            }
+        }
+
+        private void txtSDT_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool isBackspace = e.KeyChar == '\b';
+            if (!char.IsDigit(e.KeyChar) && !isBackspace)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtSDT_TextChanged(object sender, EventArgs e)
+        {
+            if(txtSDT.Text == "")
+            {
+                lblSDT.ForeColor = Color.Red;
+                lblSDT.Text = "Số điện thoại không được bỏ trống";
+            }else if(txtSDT.Text.Length != 10)
+            {
+                lblSDT.ForeColor = Color.Red;
+                lblSDT.Text = "";
+            }
+            else
+            {
+                lblSDT.ForeColor = Color.Green;
+                lblSDT.Text = "Số điện thoại hợp lệ";
+            }
+        }
+
+        private void txtHoTen_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool isBackspace = e.KeyChar == '\b';
+            if (!char.IsLetter(e.KeyChar) && !isBackspace && !char.IsWhiteSpace(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
